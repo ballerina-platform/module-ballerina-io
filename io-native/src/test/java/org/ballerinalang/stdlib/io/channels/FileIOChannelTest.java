eee73114f92e2d15b74bcbc74ef1d67ff3e7d6fb
BuddhiWathsala <b.wathsala.bw@gmail.com>
2021-05-07 14:45:34 +0530

    Minor doc comment fix

--- io-native/src/test/java/org/ballerinalang/stdlib/io/channels/FileIOChannelTest.java
+++ io-native/src/test/java/org/ballerinalang/stdlib/io/channels/FileIOChannelTest.java
@@ -31,7 +31,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- * The unit test of the FileIOChannelTest class.
+ * The unit test of the FileIOChannel class.
  */
 public class FileIOChannelTest {
 
