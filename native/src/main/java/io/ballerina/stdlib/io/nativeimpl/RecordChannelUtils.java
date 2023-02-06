5480cfbcad5628e905461c0287c6d25a8b0acb86
Sahan Hemachandra <45299562+sahanHe@users.noreply.github.com>
2023-02-06 16:49:27 +0530

    Update code to specifically support records and string array of arrays in CSV red

--- native/src/main/java/io/ballerina/stdlib/io/nativeimpl/RecordChannelUtils.java
+++ native/src/main/java/io/ballerina/stdlib/io/nativeimpl/RecordChannelUtils.java
@@ -174,7 +174,7 @@ public class RecordChannelUtils {
                 }
                 Object[] out = outList.toArray();
                 return ValueCreator.createArrayValue(out, TypeCreator.createArrayType(describingType));
-            } else {
+            } else if (describingType.getTag() == TypeTags.ARRAY_TAG) {
                 ArrayList<BArray> outList = new ArrayList<>();
                 while (textRecordChannel.hasNext()) {
                     String[] record = textRecordChannel.read();
@@ -186,6 +186,8 @@ public class RecordChannelUtils {
                 }
                 Object[] out = outList.toArray();
                 return ValueCreator.createArrayValue(out, TypeCreator.createArrayType(describingType));
+            } else {
+                return IOUtils.createError("Unsupported mapping type detected. ");
             }
         } catch (BallerinaIOException e) {
             return IOUtils.createError(e);
